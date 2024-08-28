d78f445bce1b0e186dd73aefad920f4489774fc8
kaikaci12 <somewonesomewone@gmail.com>
2024-08-28 18:40:05 +0400

    add utcDate and unix date convertion

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 // where your node app starts
 
 // init project
-import e from "express";
-const app = e();
+var express = require("express");
+var app = express();
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require("cors");
@@ -11,6 +12,7 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
+app.use(express.json());
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
@@ -18,8 +20,16 @@ app.get("/", function (req, res) {
 });
 
 // your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+app.get("/api/:date", (req, res) => {
+  const paramsDate = req.params.date;
+  if (!paramsDate) {
+    return res.status(401).json({ msg: "please provide date" });
+  }
+  const utcDate = new Date(paramsDate).toUTCString();
+  const unixTimestamp = Math.floor(new Date(paramsDate).getTime() / 1000);
+  res.status(200).json({ unixTimestamp, utcDate });
+  console.log("UTC Date:", utcDate);
+  console.log("Unix Timestamp:", unixTimestamp);
 });
 
 // Listen on port set in environment variable or default to 3000
